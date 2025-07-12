1bf8abb4ebd7817d128a213833c9b46470d69a45
Lee Dong Wook <sh95119@gmail.com>
2025-07-12 11:46:37 +0900

    init

---
+++ monad_app/lib/monad_app_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule MonadAppWeb.PageController do
+  use MonadAppWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end

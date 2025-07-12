4baad1683015aa1598c129299d09881dcc30a2f7
Lee Dong Wook <sh95119@gmail.com>
2025-07-12 16:26:51 +0900

    feat: implement user auth

--- monad_app/lib/monad_app_web/router.ex
+++ monad_app/lib/monad_app_web/router.ex
@@ -1,6 +1,8 @@
 defmodule MonadAppWeb.Router do
   use MonadAppWeb, :router
 
+  import MonadAppWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,7 +10,20 @@ defmodule MonadAppWeb.Router do
     plug :put_root_layout, html: {MonadAppWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
     plug Inertia.Plug
+  end
+
+  pipeline :authenticated do
+    plug MonadAppWeb.AuthPlug, :user_required
+  end
+
+  pipeline :unauthenticated do
+    plug MonadAppWeb.AuthPlug, :no_user
+  end
+
+  pipeline :auth_optional do
+    plug MonadAppWeb.AuthPlug, :user_optional
   end
 
   pipeline :api do
@@ -16,9 +31,19 @@ defmodule MonadAppWeb.Router do
   end
 
   scope "/", MonadAppWeb do
-    pipe_through :browser
+    pipe_through [:browser, :auth_optional]
 
     get "/", PageController, :home
+  end
+
+  scope "/", MonadAppWeb do
+    pipe_through [:browser, :unauthenticated]
+
+    get "/register", RegisterController, :index
+    post "/register", RegisterController, :register
+
+    get "/login", LoginController, :index
+    post "/login", LoginController, :login
   end
 
   # Other scopes may use custom stacks.
@@ -41,5 +66,23 @@ defmodule MonadAppWeb.Router do
       live_dashboard "/dashboard", metrics: MonadAppWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", MonadAppWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", MonadAppWeb do
+    pipe_through [:browser, :require_authenticated_user]
+  end
+
+  scope "/", MonadAppWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
   end
 end
